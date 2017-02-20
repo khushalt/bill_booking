c33a4e4bb9c0c8da6991a0f4595dcbd4d39682be
sbkolate <kolate.sambhajI@gmail.com>
2017-02-20 15:15:49 +0530

    CHANGED THE PAYMENT DETAILS REPORT

--- bill_booking_application/bill_booking_application/report/payment_details/payment_details.py
+++ bill_booking_application/bill_booking_application/report/payment_details/payment_details.py
@@ -12,8 +12,7 @@ def execute(filters=None):
 	return columns, data
 
 def get_data(filters):
-	#print filters.from_date,filters.to_date
-	if filters.from_date and filters.to_date:
+	if filters.from_date and filters.to_date and filters.select_type:
 		return frappe.db.sql(""" select vendor_entry,
 										invoice_no,
 										booking_date,
@@ -22,28 +21,17 @@ def get_data(filters):
 										balance_amount 
 									from `tabBook Bill` 
 										where 
-	(booking_date between '{0}' and '{1}')""".format(filters.from_date,filters.to_date,get_condition(filters.select_type)),as_list=1,debug=1)
+	(booking_date between '{0}' and '{1}')and {2}""".format(filters.from_date,filters.to_date,get_condition(filters.select_type)),as_list=1,debug=1)
 	else:
 		return []	
-	# if filters.from_date:
-	# 	if filters.to_date:
-			
-	# 		if filters.select_type=="Payment Done":
-				# return frappe.db.sql(""" select vendor_entry,invoice_no,booking_date,
-				# 	bill_amount,bill_details,balance_amount from `tabBook Bill` 
-				# 	where balance_amount=0 and (booking_date between {0}  {1}) """.format(filters.from_date,filters.to_date),as_list=1)
-			
-	# 		elif filters.select_type=="Payment Due":
-	# 			return frappe.db.sql("""select vendor_entry,invoice_no,booking_date,
-	# 				bill_amount,bill_details,balance_amount from `tabBook Bill`
-	# 				where balance_amount>0 and (booking_date between {0} {1}) """.format(filters.from_date,filters.to_date),as_list=1)
-			
-	# 			# frappe.throw(_("Enter Payment Type"))
-		
 	
-	# 	# frappe.throw(_("please Enter From Date"))
-
-
+			
+def get_condition(filters_):
+	if filters_:
+		if filters_=="Payment Done":
+			return "balance_amount=0"
+		else:
+			return "balance_amount>0"
 
 def get_colums():
 	columns = [("Vendor Entry") + ":Link/Supplier:100"] +\
@@ -52,7 +40,6 @@ def get_colums():
 			  [("Bill Amount") + ":Data:100"] + \
 			  [("Bill Details") + ":Data:500"] + \
 			  [("Balance") + ":Currency:120"] 
-			  
 	return columns	
 
 
